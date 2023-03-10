44afa4c9cd95fb5a4f7d89e4bc681329dc5588fd
AnaPaulaJuarez <anapaulajuarezred@hotmail.com>
2023-03-10 17:23:00 -0600

    Archivos

--- data.py
+++ data.py
@@ -1,13 +1,18 @@
 
 import pandas as pd
 import numpy as np
+import os
 
-def Lectura_Datos_Naftrac(fecha: str) :
+def Lectura_Datos_Naftrac(fecha: str):
     
-    # Leemos un archivo csv con el nombre "NAFTRAC_" concatenado con el argumento `fecha` y la extensión ".csv"
-    # Omitimos las primeras dos filas del archivo y eliminamos todas las filas que contengan valores faltantes (NaN)
-    # El resultado se asigna a una variable llamada `datos`
-    datos = pd.read_csv("NAFTRAC_" + fecha + ".csv", skiprows=2).dropna()
+    # Definimos la ruta de la carpeta donde están los archivos
+    folder_path = "Files"
+    
+    # Combinamos la ruta de la carpeta con el nombre del archivo
+    file_path = os.path.join(folder_path, "NAFTRAC_" + fecha + ".csv")
+    
+    # Leemos el archivo csv y lo guardamos en la variable `datos`
+    datos = pd.read_csv(file_path, skiprows=2).dropna()
     
     # Devolvemos la variable `datos`, que contiene los datos del archivo csv limpios, al llamador de la función
     return datos
